8d85561ae30a35b243c11df79c806fc346c57cdc
VladDocCoder <vovadoc0@gmail.com>
2017-03-25 02:57:05 +0200

    "Initial commit"

--- app/serve/js/app.js
+++ app/serve/js/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var myApp = angular.module('myApp',[
+var myApp = angular
+    .module('myApp',[
     'ngStorage', 'toastr', 'ngRoute'
-]).config(["$routeProvider", function($routeProvider) {
+])
+    .config(["$routeProvider", function($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "./index.html",
         controller: "ItemController"
     });
-} ]);
+} ])
+    .run(['$rootScope', function($rootScope){
+        $rootScope.$on($rootScope);
+    }]);
 
 
 
